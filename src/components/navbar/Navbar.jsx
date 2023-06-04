cd3f8a3d050a7d99e05f760969a7bcc7f5136423
Mauricio Guerrero <maurizioguerrero@hotmail.com>
2023-06-04 16:10:20 -0500

    update

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,28 +1,40 @@
 import React from "react";
-import './Navbar.css'
+import "./Navbar.css";
 
 const Navbar = () => {
-
   return (
     <nav className="nav">
-        <div className="nav__container">
-            <h1 className="nav__logo">Dr. Mauricio Guerrero R.</h1>
-            
-            <label htmlFor="menu" className="nav__label">
-                <img src="../../assets/imagenes/menu.svg" alt="" className="nav__img" />
-            </label>
-            <input type="checkbox" id="menu" className="nav__input"/>
-            <div className="nav__menu">
-                <a className="nav__item" href="/">Inicio</a>
-                <a className="nav__item" href="/portafolio">Portafolio</a>
-                <a className="nav__item" href="/enfermedades">Enfermedades</a>
-                <a className="nav__item" href="/medicamentos">Medicamentos</a>
-                <a className="nav__item" href="/Contacto">Contacto</a>
-            </div>
+      <div className="nav__container">
+        <h1 className="nav__logo">Dr. Mauricio Guerrero R.</h1>
+
+        <label htmlFor="menu" className="nav__label">
+          <img
+            src="../../assets/imagenes/menu.svg"
+            alt=""
+            className="nav__img"
+          />
+        </label>
+        <input type="checkbox" id="menu" className="nav__input" />
+        <div className="nav__menu">
+          <a className="nav__item" href="/">
+            Inicio
+          </a>
+          <a className="nav__item" href="/portafolio">
+            Portafolio
+          </a>
+          <a className="nav__item" href="/enfermedades">
+            Enfermedades
+          </a>
+          <a className="nav__item" href="/medicamentos">
+            Medicamentos
+          </a>
+          <a className="nav__item" href="/Contacto">
+            Contacto
+          </a>
         </div>
+      </div>
     </nav>
   );
-}
-
+};
 
 export default Navbar;
