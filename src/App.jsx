09ef23e46c452eadb52f6aa479dfd89e9106040f
Mauricio Guerrero <maurizioguerrero@hotmail.com>
2023-06-05 09:03:47 -0500

    up

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,38 @@
-import React from "react";
-import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
-import Inicio from "./components/Inicio/Inicio"
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import  Inicio from "./routes/Inicio/Inicio"
+import Medicamentos from './routes/Medicamentos/Medicamentos'
+import Contacto from './routes/Contacto/Contacto'
+import Portafolio from './routes/Portafolio/Portafolio'
+import Enfermedades from './routes/Enfermedades/Enfermedades'
 
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Inicio/>,
+    errorElement: <h1>Ruta Incorrecta</h1>
+  },
+  {
+    path: '/medicamentos',
+    element: <Medicamentos/>,
+  },
+  {
+    path: '/portafolio',
+    element: <Portafolio/>,
+  },
+  {
+    path: '/enfermedades',
+    element: <Enfermedades/>,
+  },
+  {
+    path: '/contacto',
+    element: <Contacto/>,
+  },
+]);
 
-function App() {
-  
-  return (
-    <Router>
-      <div className="App">Hola
-      
-      </div>
-    </Router>
-  )
-}
-
-export default App
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <RouterProvider router={router}/>
+  </React.StrictMode>,
+)
